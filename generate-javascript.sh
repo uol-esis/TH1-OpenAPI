97734dea658953a77c7fbff32ccd5202fd9ba36d
Tilman Holube <tilman@holube.de>
2025-04-08 10:29:17 +0200

    add scripts for server and client

--- generate-javascript.sh
+++ generate-javascript.sh
@@ -5,6 +5,5 @@ docker run --rm \
   -u "${UID}":"${GID}" \
   -v "${PWD}":/local openapitools/openapi-generator-cli:v7.12.0 generate \
   -i /local/openapi.yaml \
-  -g spring \
-  --additional-properties=useSpringBoot3=true \
-  -o /local/target
+  -g javascript \
+  -o /local/target/javascript
