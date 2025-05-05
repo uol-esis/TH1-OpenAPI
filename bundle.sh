7668f77601582c090aa85eb3d58c99f0704f96b8
Tilman Holube <tilman@holube.de>
2025-05-05 14:46:49 +0200

    test merging with openapi generator

---
+++ bundle.sh
@@ -0,0 +1,12 @@
+UID=$(id -u)
+GID=$(id -g)
+
+docker run --rm \
+  -u "${UID}":"${GID}" \
+  -v "${PWD}":/local openapitools/openapi-generator-cli:v7.13.0 generate \
+  -i /local/src/openapi.yaml \
+  -g openapi-yaml \
+  -o /local/target/bundle\
+  --skip-validate-spec
+
+cp target/bundle/openapi/openapi.yaml ./openapi.yaml
