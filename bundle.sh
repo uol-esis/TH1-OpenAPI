d4975c66d058d52919b828524e9c19f65aba6282
Tilman Holube <tilman@holube.de>
2025-05-08 11:12:41 +0200

    22 split openapi file tilman (#27)
    * test merging with openapi generator
    
    * moving things around
    
    * readd converterType property
    
    * bundle
    
    * Update README.md
    
    * removed default line break due to issues in js client

--- bundle.sh
+++ bundle.sh
@@ -4,6 +4,8 @@ GID=$(id -g)
 docker run --rm \
   -u "${UID}":"${GID}" \
   -v "${PWD}":/local openapitools/openapi-generator-cli:v7.13.0 generate \
-  -i /local/openapi.yaml \
-  -g javascript \
-  -o /local/target/javascript
+  -i /local/src/openapi.yaml \
+  -g openapi-yaml \
+  -o /local/target/bundle
+
+cp target/bundle/openapi/openapi.yaml ./openapi.yaml
